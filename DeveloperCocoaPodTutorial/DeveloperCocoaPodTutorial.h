c87581a24ab7219ade139db5fa85dd655192589a
12dot <imireshkin@65apps.com>
2021-11-11 22:16:13 +0300

    Initial Commit

---
+++ DeveloperCocoaPodTutorial/DeveloperCocoaPodTutorial.h
@@ -0,0 +1,18 @@
+//
+//  DeveloperCocoaPodTutorial.h
+//  DeveloperCocoaPodTutorial
+//
+//  Created by Ilya Mireshkin on 11.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeveloperCocoaPodTutorial.
+FOUNDATION_EXPORT double DeveloperCocoaPodTutorialVersionNumber;
+
+//! Project version string for DeveloperCocoaPodTutorial.
+FOUNDATION_EXPORT const unsigned char DeveloperCocoaPodTutorialVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeveloperCocoaPodTutorial/PublicHeader.h>
+
+
